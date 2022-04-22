cea075b06af9cad988a67031d5080bc120f09039
Fang Huai An <liddle.fang@gmail.com>
2022-04-23 01:36:19 +0800

    Initial Commit

---
+++ StrategistReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StrategistReader
+//
+//  Created by 房懷安 on 2022/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StrategistReaderUITests/StrategistReaderUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  StrategistReaderUITestsLaunchTests.swift
+//  StrategistReaderUITests
+//
+//  Created by 房懷安 on 2022/4/23.
+//
+
+import XCTest
+
+class StrategistReaderUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
